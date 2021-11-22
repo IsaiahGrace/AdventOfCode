e5bf9cb36761a19d8293cabb84b5df18cbc5e779
Isaiah Grace <isaiah.grace@sestrasystems.com>
2021-11-21 23:22:40 -0500

    Added spdlog library

--- 2019/5/main.cpp
+++ 2019/5/main.cpp
@@ -7,6 +7,8 @@
 #include <fstream>
 
 #include "opcodes.hpp"
+#include "spdlog/spdlog.h"
+#include "spdlog/cfg/env.h"
 
 void initMem(std::vector<size_t>& mem, std::ifstream& inputStream) {
 	while (inputStream.good()) {
@@ -17,6 +19,15 @@ void initMem(std::vector<size_t>& mem, std::ifstream& inputStream) {
 	}
 }
 
+void logMem(std::vector<size_t>& mem, std::string opcode, size_t pc, size_t num) {
+	std::stringstream info;
+	info << opcode << ": ";
+	for (size_t i = 0; i < num; i++) {
+		info << mem[pc + i] << " ";
+	}
+	spdlog::info(info.str());
+}
+
 size_t exec(std::vector<size_t>& mem, size_t pc) {
 	size_t opcode = mem[pc] % 100;
 	size_t options = mem[pc] / 100;
@@ -25,17 +36,14 @@ size_t exec(std::vector<size_t>& mem, size_t pc) {
 	char optB = (options / 10) % 10;
 	//char optC = options / 100;
 
-	//std::cout << "A : " << (optA ? "Imm " : "Pos ");
-	//std::cout << "B : " << (optB ? "Imm " : "Pos ");
-	//std::cout << "OP: " << opcode << " ";
-
+	std::stringstream info;
 	switch (opcode) {
 		case ADD: {
 			size_t a = optA ? mem[pc + 1] : mem[mem[pc + 1]];
 		 	size_t b = optB ? mem[pc + 2] : mem[mem[pc + 2]];
 			size_t dest = mem[pc + 3];
 			mem[dest] = a + b;
-			//std::cout << "mem[" << dest << "] = " << a << " + " << b << std::endl;
+			logMem(mem, "ADD", pc, 4);
 			pc += 4;
 			break;
 		}
@@ -44,55 +52,50 @@ size_t exec(std::vector<size_t>& mem, size_t pc) {
 			size_t b = optB ? mem[pc + 2] : mem[mem[pc + 2]];
 			size_t dest = mem[pc + 3];
 			mem[dest] = a * b;
-			//std::cout << "mem[" << dest << "] = " << a << " * " << b << std::endl;
+			logMem(mem, "MUL", pc, 4);
 			pc += 4;
 			break;
 		}
 		case READ: {
 			std::cout << "Enter a number: ";
 			std::cin >> (optA ? mem[pc + 1] : mem[mem[pc + 1]]);
-			//std::cout << "Read from io: " << (optA ? mem[pc + 1] : mem[mem[pc + 1]]) << std::endl;
+			logMem(mem, "READ", pc, 2);
 			pc += 2;
 			break;
 		}
 		case WRITE: {
 			std::cout << (optA ? mem[pc + 1] : mem[mem[pc + 1]]) << std::endl;
+			logMem(mem, "WRITE", pc, 2);
 			pc += 2;
 			break;
 		}
 		case JNZ: {
 			size_t a = optA ? mem[pc + 1] : mem[mem[pc + 1]];
 			size_t dest = optB ? mem[pc + 2] : mem[mem[pc + 2]];
-			//std::cout << "JNZ ";
+			logMem(mem, "JNZ", pc, 2);
 			if (a) {
-				//std::cout << "Jumping to " << dest;
 				pc = dest;
 			} else {
-				//std::cout << "Not jumping";
 				pc += 3;
 			}
-			//std::cout << std::endl;
 			break;
 		}
 		case JZ: {
 			size_t a = optA ? mem[pc + 1] : mem[mem[pc + 1]];
 			size_t dest = optB ? mem[pc + 2] : mem[mem[pc + 2]];
-			//std::cout << "JZ ";
+			logMem(mem, "JZ", pc, 2);
 			if (a) {
-				//std::cout << "Not jumping";
 				pc += 3;
 			} else {
-				//std::cout << "Jumping to " << dest;
 				pc = dest;
 			}
-			//std::cout << std::endl;
 			break;
 		}
 		case LT: {
 			size_t a = optA ? mem[pc + 1] : mem[mem[pc + 1]];
 			size_t b = optB ? mem[pc + 2] : mem[mem[pc + 2]];
 			mem[mem[pc + 3]] = (a < b);
-			//std::cout << "LT : (" << a << " < " << b << ") == " << (a < b) << std::endl;
+			logMem(mem, "LT", pc, 4);
 			pc += 4;
 			break;
 		}
@@ -100,16 +103,15 @@ size_t exec(std::vector<size_t>& mem, size_t pc) {
 			size_t a = optA ? mem[pc + 1] : mem[mem[pc + 1]];
 			size_t b = optB ? mem[pc + 2] : mem[mem[pc + 2]];
 			mem[mem[pc + 3]] = (a == b);
-			//std::cout << "CMP : (" << a << " == " << b << ") == " << (a == b) << std::endl;
+			logMem(mem, "CMP", pc, 4);
 			pc += 4;
 			break;
 		}
 		case HALT: {
-			//std::cout << std::endl;
 			break;
 		}
 		default: {
-			std::cerr << "exec unknown instruction: pc = " << pc << " opcode = " << opcode << std::endl;
+			spdlog::error("exec unknown instruction: pc = {} opcode = {}", pc, opcode);
 			break;
 		}
 	}
@@ -138,6 +140,8 @@ int main (int argc, char** argv) {
 		std::cerr << "Please provide an input file" << std::endl;
 		return EXIT_FAILURE;
 	}
+
+	spdlog::cfg::load_env_levels();
 
 	std::ifstream inputStream(argv[1], std::ifstream::in);
 	std::vector<size_t> mem;
