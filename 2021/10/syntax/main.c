d40e2d8e0691b6e33b8198feeb04a26dd7d5eac3
Isaiah Grace <isaiah@graces.com>
2022-01-16 13:10:51 -0500

    Solved 2021/10-1

--- 2021/10/syntax/main.c
+++ 2021/10/syntax/main.c
@@ -32,15 +32,112 @@ char *read_file(char* file_path) {
 
 	if (ferror(fp)) {
 		printf("Error reading file.\n");
-	} else {
-		buffer[bytes_read] = '\0';
+		free(buffer);
+		return NULL;
 	}
+
+	// Ensure that the buffer data is null-terminated
+	buffer[bytes_read] = '\0';
 
 	fclose(fp);
 
 	return buffer;
 }
 
+int pop(char* position, char expected_opener) {
+	// We'll use the buffer itself to keep track of which characters we've matched.
+	// By setting the most significant bit in the character, we will signify that the character has already been matched.
+	// This is a little bit like a stack. The matched characters are just ignored. We expect the stack head to be equal to the expected_opener.
+
+
+	// The current position becomes matched:
+	*position |= 0x80;
+
+	// Skip over characters we've already matched:
+	while (*position & 0x80) {
+		position--;
+	}
+
+	// This is the most recent character that has not been matched.
+	char opener = *position;
+
+	// Set the most significatnt bit, matching it!
+	*position |= 0x80;
+
+	// Return true if the opener was expected.
+	return (opener == expected_opener);
+}
+
+int pt1_answer(char *buffer) {
+	if (buffer == NULL) {
+		printf("Error, pt1_answer was given an input struct with a NULL pointer.\n");
+		return 0;
+	}
+
+	// Start at the begining
+	char *position = buffer;
+
+	int score = 0;
+
+	// Loop over all lines
+	while (*position != '\0') {
+
+		// Loop over characters in the lines
+		while (*position != '\n') {
+
+			char current = *position;
+			//printf("%c", current);
+
+			// if a closing symbol is found, find a match for it.
+			switch (*position) {
+				case ')': {
+					if (!pop(position, '(')) {
+						score += 3;
+						//printf(" Corrupted line! Unexpected ).");
+						// Fast forward to the end of the line
+						while (*(position + 1) != '\n') position++;
+					}
+					break;
+				}
+				case ']': {
+					if (!pop(position, '[')) {
+						score += 57;
+						//printf(" Corrupted line! Unexpected ].");
+						while (*(position + 1) != '\n') position++;
+					}
+					break;
+				}
+				case '}': {
+					if (!pop(position, '{')) {
+						//printf(" Corrupted line! Unexpected }.");
+						score += 1197;
+						while (*(position + 1) != '\n') position++;
+					}
+					break;
+				}
+				case '>': {
+					if (!pop(position, '<')) {
+						//printf(" Corrupted line! Unexpected >.");
+						score += 25137;
+						while (*(position + 1) != '\n') position++;
+					}
+					break;
+				}
+			}
+			position++;
+		}
+		position++;
+		//printf("\n");
+	}
+
+	// We'll unset the highest bits in the buffer, resetting it.
+	// It would be cool to try to use SIMD instructions here, but we don't know the length of the buffer...
+	while (*buffer != '\0') {
+		*buffer++ &= 0x7F;
+	}
+
+	return score;
+}
 
 int main(int argc, char** argv) {
 	if (argc < 2) {
@@ -54,5 +151,9 @@ int main(int argc, char** argv) {
 		return EXIT_FAILURE;
 	}
 
-	printf("%s",buffer);
+	//printf("%s",input.buffer);
+
+	int pt1 = pt1_answer(buffer);
+	printf("Part 1 score: %d\n", pt1);
+	//printf("%s",buffer);
 }
