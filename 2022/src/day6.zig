74fe926e7a653823e41c6bfa589285712f9dff34
Isaiah Grace <irgkenya4@gmail.com>
2023-01-02 20:00:09 -0500

    Added an alternate solution for day 6 using std.StaticBitSet

--- 2022/src/day6.zig
+++ 2022/src/day6.zig
@@ -47,3 +47,35 @@ test "part2" {
     try std.testing.expectEqual(try solveP2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 29);
     try std.testing.expectEqual(try solveP2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 26);
 }
+
+// After I solved day 6, I found the standard library StaticBitSet, which is exactly what I needed!
+// This is still probably not the fastest way to solve this puzzle. I tried to use XOR at first,
+// but couldn't get it to work, and then I read a blog post explaining how to solve it with XOR...
+fn solveNBitSet(input: []const u8, count: usize) !u32 {
+    var i: usize = 0;
+    while (i < input.len - count) : (i += 1) {
+        var bitSet = std.StaticBitSet(256).initEmpty();
+        var j: usize = 0;
+        while (j < count) : (j += 1) {
+            bitSet.set(input[i + j]);
+        }
+        if (bitSet.count() == count) {
+            return @intCast(u32, i + count);
+        }
+    }
+    return error.InvalidPuzzleInput;
+}
+
+test "std.StaticBitSet" {
+    try std.testing.expectEqual(try solveN("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4), try solveNBitSet("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4));
+    try std.testing.expectEqual(try solveN("bvwbjplbgvbhsrlpgdmjqwftvncz", 4), try solveNBitSet("bvwbjplbgvbhsrlpgdmjqwftvncz", 4));
+    try std.testing.expectEqual(try solveN("nppdvjthqldpwncqszvftbrmjlhg", 4), try solveNBitSet("nppdvjthqldpwncqszvftbrmjlhg", 4));
+    try std.testing.expectEqual(try solveN("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4), try solveNBitSet("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4));
+    try std.testing.expectEqual(try solveN("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4), try solveNBitSet("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4));
+
+    try std.testing.expectEqual(try solveN("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14), try solveNBitSet("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14));
+    try std.testing.expectEqual(try solveN("bvwbjplbgvbhsrlpgdmjqwftvncz", 14), try solveNBitSet("bvwbjplbgvbhsrlpgdmjqwftvncz", 14));
+    try std.testing.expectEqual(try solveN("nppdvjthqldpwncqszvftbrmjlhg", 14), try solveNBitSet("nppdvjthqldpwncqszvftbrmjlhg", 14));
+    try std.testing.expectEqual(try solveN("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14), try solveNBitSet("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14));
+    try std.testing.expectEqual(try solveN("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14), try solveNBitSet("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14));
+}
