8d8802b2794913608d867ecf73785f96f4a4531b
Isaiah Grace <irgkenya4@gmail.com>
2023-12-07 00:16:28 -0500

    Solved 2023/06 part 2

--- 2023/src/day06.rs
+++ 2023/src/day06.rs
@@ -21,6 +21,8 @@ fn solve_poly_roots(b: i64, c: i64) -> Range<i64> {
 pub struct Day06 {
     times: Vec<i64>,
     dists: Vec<i64>,
+    p2_time: i64,
+    p2_dist: i64,
 }
 
 impl Day06 {
@@ -34,7 +36,8 @@ impl Day06 {
     }
 
     fn solve_p2(&self) -> i64 {
-        0
+        let range = solve_poly_roots(self.p2_time, -self.p2_dist);
+        range.end - range.start
     }
 }
 
@@ -58,6 +61,27 @@ impl From<String> for Day06 {
                 .skip(1)
                 .map(|n| n.parse::<i64>().unwrap())
                 .collect(),
+            p2_time: input
+                .lines()
+                .next()
+                .unwrap()
+                .strip_prefix("Time:")
+                .unwrap()
+                .split_whitespace()
+                .collect::<String>()
+                .parse::<i64>()
+                .unwrap(),
+            p2_dist: input
+                .lines()
+                .skip(1)
+                .next()
+                .unwrap()
+                .strip_prefix("Distance:")
+                .unwrap()
+                .split_whitespace()
+                .collect::<String>()
+                .parse::<i64>()
+                .unwrap(),
         }
     }
 }
@@ -82,7 +106,10 @@ mod tests {
     #[test]
     fn file_01() {
         let mut solver: Day06 = std::fs::read_to_string("06/01").unwrap().into();
-        assert_eq!(solver.solve().unwrap(), crate::Solution::Integer(288, 0));
+        assert_eq!(
+            solver.solve().unwrap(),
+            crate::Solution::Integer(288, 71503)
+        );
     }
 
     #[test]
@@ -90,7 +117,7 @@ mod tests {
         let mut solver: Day06 = std::fs::read_to_string("06/input").unwrap().into();
         assert_eq!(
             solver.solve().unwrap(),
-            crate::Solution::Integer(4811940, 0)
+            crate::Solution::Integer(4811940, 30077773)
         );
     }
 }
